6ab42bb423ef9f04d461fa469c11a331bd7c7f0a
willyfreeman <947521179@qq.com>
2019-10-18 04:23:19 +0800

    This is the message describing the commit

---
+++ config/base.php
@@ -0,0 +1,104 @@
+<?php return array (
+  'site_name' => 'OneIndex',
+  'password' => '25245769',
+  'style' => 'material',
+  'onedrive_root' => '',
+  'cache_type' => 'secache',
+  'cache_expire_time' => 3600,
+  'cache_refresh_time' => 600,
+  'root_path' => '?',
+  'show' => 
+  array (
+    'stream' => 
+    array (
+      0 => 'txt',
+    ),
+    'image' => 
+    array (
+      0 => 'bmp',
+      1 => 'jpg',
+      2 => 'jpeg',
+      3 => 'png',
+      4 => 'gif',
+    ),
+    'video5' => 
+    array (
+      0 => 'mp4',
+      1 => 'webm',
+      2 => 'mkv',
+    ),
+    'video' => 
+    array (
+    ),
+    'video2' => 
+    array (
+      0 => 'avi',
+      1 => 'mpg',
+      2 => 'mpeg',
+      3 => 'rm',
+      4 => 'rmvb',
+      5 => 'mov',
+      6 => 'wmv',
+      7 => 'asf',
+      8 => 'ts',
+      9 => 'flv',
+    ),
+    'audio' => 
+    array (
+      0 => 'ogg',
+      1 => 'mp3',
+      2 => 'wav',
+    ),
+    'code' => 
+    array (
+      0 => 'html',
+      1 => 'htm',
+      2 => 'php',
+      3 => 'css',
+      4 => 'go',
+      5 => 'java',
+      6 => 'js',
+      7 => 'json',
+      8 => 'txt',
+      9 => 'sh',
+      10 => 'md',
+    ),
+    'doc' => 
+    array (
+      0 => 'csv',
+      1 => 'doc',
+      2 => 'docx',
+      3 => 'odp',
+      4 => 'ods',
+      5 => 'odt',
+      6 => 'pot',
+      7 => 'potm',
+      8 => 'potx',
+      9 => 'pps',
+      10 => 'ppsx',
+      11 => 'ppsxm',
+      12 => 'ppt',
+      13 => 'pptm',
+      14 => 'pptx',
+      15 => 'rtf',
+      16 => 'xls',
+      17 => 'xlsx',
+    ),
+  ),
+  'images' => 
+  array (
+    'home' => false,
+    'public' => false,
+    'exts' => 
+    array (
+      0 => 'jpg',
+      1 => 'png',
+      2 => 'gif',
+      3 => 'bmp',
+    ),
+  ),
+  'client_secret' => 'acaRTLG12)-?rqmrYUH257(',
+  'client_id' => 'ed79a735-4bf6-4c51-8b07-9832cdc7335a',
+  'redirect_uri' => 'https://oneindex.github.io/',
+  'refresh_token' => 'OAQABAAAAAACQN9QBRU3jT6bcBQLZNUj7QaBZEM9ExwRGCNFYfkYAfkfbS1iPpAKQK7HSuDkEgEs_AcPHCHItXCzlDSlEHvdF-vxGHRYuSPZTBNycQ_sdu3qzFTQS4RHU1CwgQSLe0JRE0hlMASrBXZ4PqUMlX7aLy9XfotRths-pck_QaD3S5tUs_kEHDfMPbanlDo-1X47j6fWotxvIKHIcYdPI6wiiPVyQSld4iLpXr9vJA6DUDYiFE2Cm50imOLxc_0UjuJCzk9GQeauGh0ki2JPwi8JtnxyycYprcZ9lsoIzxLrCDSWNkv92gvgsZgikljGHEuqN-DBjbRdVp66fF3oB9QIINVhJ1yWgaIF6OGkC2nYOGSUjQ_57B4Hg_nj4_Nz8jcX1LGQvkJnTOyHxOF5EHutffHXDygO_BozfkifrTtbaJYOKzXoVhRUvj577oN78Yi5XEhUNdVKZtSb-1D00r3YtBNq-YUMiK3Gek-w5TQdmlHpfez6iWFQfxA9Et1rH_1mh6mPiov3MD_IiXYfV31ovJ0cr9yiZJQ2HMCI2QMUqDumoiJN0eOo8YUR1OvCWqy6c8PvVjE6Ab3PnM7jYAnEpjxQDGTy6GrVB79T6S2fFIgJsvFX3Qo2Vwq-rnoHs_Ff2drIL81MOp-QtfuxAwPe1jtRy6bpZcBf_-n2ufzJcyG34o9K1fmAxg0utn-n6_-nWJai-e8evs0uKf0lpB355IFrNVyibWati9YRvEeG7S2zxo-eDIExT6XmVq_YW7862I1NGfaIP6T3RkjCBHDuaYMmjMxFQnVXAsBNbED8WXb_kt1dBFLqD2QjOSEDsSxBzUd9IFP9Bad5foQq6p8g7IAA',
+);
